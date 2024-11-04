13e809326dcc23fb63351582c5d172bbbafcbf19
Alessandra Azevedo <nava.31105@granitopagamentos.com.br>
2024-11-04 17:20:53 -0300

    Ajustes Myahgora 2024

--- Move.py
+++ Move.py
@@ -3,30 +3,28 @@
 
 # In[21]:
 
-from calendar import weekday
 import pyautogui as py
 import time
-import pyperclip
 import datetime
+import webbrowser
 
 py.PAUSE=1
 
 E1 = datetime.time(8,4).strftime("%H:%M")
 S1 = datetime.time(12,20).strftime("%H:%M")
 E2 = datetime.time(13,15).strftime("%H:%M")
-S2 = datetime.time(18,0).strftime("%H:%M")
+S2 = datetime.time(18,9).strftime("%H:%M")
+url = "https://www.ahgora.com.br/novabatidaonline/?defaultDevice=a524560"
 
 #abrir busca do windows
 py.press("winleft")
 
 #digitar Notepad
-py.write("notepad")
+py.write("bloco de notas")
 py.press("enter")
 
 #maximizar notepad
-with py.hold('alt'): 
-    py.press("space")
-    py.press("x")
+py.hotkey('win', 'up')
 
 
 # Abrir arquivo
@@ -43,74 +41,64 @@ while True:
             if current_time == E1 or current_time == S1 or current_time == E2 or current_time == S2:
                 
                 py.write("Aguarde...Data: " + current_date + " - Hora: " + datetime.datetime.now().strftime("%H:%M:%S"))
+                py.press("enter")
                
                 #Código marcação de ponto
 
-                #Abrir Chrome 
-                py.press("winleft")
-                py.write("Chrome")
-                py.press("enter")
-                time.sleep(2)   
+                #Abrir Url 
+                webbrowser.open(url)
+                time.sleep(5)   
                 
                 #maximiza tela
-                with py.hold('alt'): 
-                        py.press("space")
-                        py.press("x")
+                py.hotkey('win', 'up')                
                 time.sleep(1)
                 
-                #clica no perfil Nava
-                py.click(x=683, y=441)
+                #pega o retorno da posicao atual de x e y do mouse e passa o valor da tupla para as duas variaveis
+                # x, y = py.position()
+                # print("Registre seu ponto:")
+                # print("x = "+str(x)+" y = "+str(y))
+
+                #clica em Registre seu ponto
+                py.click(x=1127, y=693)
+                time.sleep(3)
+                #clica em acessar via SSO
+                py.click(x=960, y=825)
+                time.sleep(5)
+
+                #pega o retorno da posicao atual de x e y do mouse e passa o valor da tupla para as duas variaveis
+                # x, y = py.position()
+                # print("Registrar ponto:")
+                # print("x = "+str(x)+" y = "+str(y))
                 
-                #testar se tela ja ta maximizada
-                with py.hold('alt'): 
-                        py.press("space")
-                        py.press("x")
-                
-                # py.click(x=1227, y=101)
-                py.click(x=182, y=162)
-                #focus na barra de endereço
-                py.click(x=397, y=51)
-
-                #Copiar e colar endereço 
-                url = "https://www.ahgora.com.br/novabatidaonline/?defaultDevice=a524560"
-                pyperclip.copy(url)
-                py.hotkey("ctrl", "v")
-                py.press("enter")
-                time.sleep(15)
-
-                #pegar position Registre seu ponto
-                py.click(x=667, y=442)
-                time.sleep(1)
-
-                #acessar via SSO
-                py.click(x=685, y=575)
-                time.sleep(10)
-
-                #escolher conta
-                py.click(x=645, y=368)
-                time.sleep(10)
-
                 #Confirmar batida
-                py.click(x=723, y=520)
-                time.sleep(8)
+                # py.click(x=1007, y=756)
+                time.sleep(5)                
 
                 #fecha browser
                 py.click(x=1339, y=12)
+                
+                #volta para notepad
+                py.hotkey('alt', 'tab')
+                time.sleep(2)
 
                 py.write("___________________________________________________")
+                py.press("enter")
                 time.sleep(60)
                 
             else :
-                py.write("Sem - Data: " + current_date + " - Hora: " + datetime.datetime.now().strftime("%H:%M:%S") + " ___________________________________________________ ")
+                py.write("Nada a marcar - Data: " + current_date + " - Hora: " + datetime.datetime.now().strftime("%H:%M:%S"))
+                py.press("enter")
                 time.sleep(8)        
     else:
         
         py.write("wkn Hora" + datetime.datetime.now().strftime("%H:%M:%S") + "Data: " + datetime.datetime.today().strftime("%A") + "(" + str(datetime.datetime.now().isoweekday()) + ")")
+        py.press("enter")
         time.sleep(8)
 
 
     # arquivo = open(r'C:/Users/T807409/Desktop/testePython.txt', "w") # Abre novamente o arquivo (escrita)
     # arquivo.writelines(conteudo)    # escreva o conteúdo criado anteriormente nele.
     # arquivo.close()            
-
+# código da empresa a524560
+# matrícula 10237
 
